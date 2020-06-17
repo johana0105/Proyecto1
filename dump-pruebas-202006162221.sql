2c20be167b9a87a1fa83b2340c36ccf63891d1b0
jgirgira <66398784+jgirgira@users.noreply.github.com>
2020-06-16 22:21:33 -0500

    Create dump-pruebas-202006162221.sql
    actualiza tabla nueva

---
+++ dump-pruebas-202006162221.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: pruebas
+-- ------------------------------------------------------
+-- Server version	5.6.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `valores`
+--
+
+DROP TABLE IF EXISTS `valores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `valores` (
+  `total` double DEFAULT NULL,
+  `porcentaje` double DEFAULT NULL,
+  `tipo_dato` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='tendra valores de ejemplo para llenar cars';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `valores`
+--
+
+LOCK TABLES `valores` WRITE;
+/*!40000 ALTER TABLE `valores` DISABLE KEYS */;
+INSERT INTO `valores` VALUES (36264,5.27,'Customers'),(5543,-1.08,'Orders'),(6254,-7.08,'Revenue'),(30.56,4.87,'Growth');
+/*!40000 ALTER TABLE `valores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-16 22:21:04
